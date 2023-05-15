fa24354e25f1270aad8a5a79698f6165c2c5d06a
Konrad Słomiany <90619862+Hagane3@users.noreply.github.com>
2023-05-15 08:48:31 +0200

    [refactor] SingleBlog: migrate to functional component #125 (#130)

--- src/views/SingleBlogs/SingleBlog.js
+++ src/views/SingleBlogs/SingleBlog.js
@@ -1,82 +1,116 @@
-import React, { Component } from "react";
-import "./SingleBlog.css"
+import React, { useEffect, useState } from "react";
+import "./SingleBlog.css";
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar";
 import data from "../../data/Blogs.json";
-import ExamplesNavbar from "../../components/Navbars/ExamplesNavbar";
+import { Spinner } from "reactstrap";
 
-class BlogsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pathid: "",
-      blogid: "",
-      loading: false,
-    };
-  }
-  
-  componentDidMount() {
-    document.documentElement.classList.remove("nav-open");
-    window.scrollTo(0, 0);
-    this.setState({ loading: true });
-    if (data) {
-      this.setState({ loading: false });
-    }
-    this.setState({ pathid: this.props.location.pathname });
-  }
-  render() {
-    let blogid = this.state.pathid.substring(
-      this.state.pathid.lastIndexOf("/") + 1
-    );
-    // console.log(blogid);
-    // console.log(data.blogsData);
-    const disp = data.blogsData.map((recievedBlog) => {
-      if (blogid === recievedBlog.blogId) {
-        return (
-          <div className="single-blog" key={recievedBlog.heading}>
-            <h1 className="blog-heading-single-page" style={{ fontSize: "3.4rem" }}>{recievedBlog.heading}</h1>
-            <h2 className="single-blog-author" style={{ fontSize: "1.8rem" }}>{recievedBlog.author}</h2>
-            <h2 className="single-blog-date" style={{ fontSize: "1.8rem" }}> {recievedBlog.date} </h2>
-            <div>
-              {recievedBlog.body.map((contentobject) => {
-                if (contentobject.type === "para") {
-                  return <p className="blogs-description text-left mb-5" key={" "}>{contentobject.content}</p>;
-                } else if (contentobject.type === "image") {
-                  return (
-                    <img
-                      className="single-blog-image"
-                      src={require("assets/img/blog/BlogImages/" +
-                        contentobject.src)}
-                      key={" "}
-                    ></img>
-                  );
-                } else if (contentobject.type === "h2") {
-                  return <h2 className="blog-secondary-heading text-left" key={" "}>{contentobject.content}</h2>;
-                } else if (contentobject.type === "list-block") {
-                  return (
-                    <div className="blog-list-block" key={" "}>
-                      <h3 className="blog-list-heading">{contentobject.listHeading}</h3>
-                      {contentobject.listItems.map((listItem) => {
-                        return <li className="blog-list-item" key={listItem}>{listItem}</li>;
-                      })}
-                    </div>
-                  );
-                } else {
-                  return;
-                }
-              })}
-            </div>
-          </div>
-        );
-      } else {
-        return;
-      }
-    });
-    return (
-      <div className="mobile-responsive">
-        <ExamplesNavbar activePage="/blogs" />
-        {disp}
-      </div>
-    );
-  }
+function SingleBlog({ location }) {
+	const [pathId, setPathId] = useState("");
+	const [blogId, setBlogId] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+		setPathId(location.pathname);
+		setBlogId(pathId.substring(pathId.lastIndexOf("/") + 1));
+		if (data) {
+			setIsLoading(false);
+		}
+	}, [pathId]);
+
+	const blogContent = data.blogsData.map((receivedBlog) => {
+		if (blogId === receivedBlog.blogId) {
+			return (
+				<div className="single-blog" key={receivedBlog.heading}>
+					<h1
+						className="blog-heading-single-page"
+						style={{ fontSize: "3.4rem" }}
+					>
+						{receivedBlog.heading}
+					</h1>
+					<h2
+						className="single-blog-author"
+						style={{ fontSize: "1.8rem" }}
+					>
+						{receivedBlog.author}
+					</h2>
+					<h2
+						className="single-blog-date"
+						style={{ fontSize: "1.8rem" }}
+					>
+						{" "}
+						{receivedBlog.date}{" "}
+					</h2>
+					<div>
+						{receivedBlog.body.map((contentobject, index) => {
+							if (contentobject.type === "para") {
+								return (
+									<p
+										className="blogs-description text-left mb-5"
+										key={`para${index}`}
+									>
+										{contentobject.content}
+									</p>
+								);
+							} else if (contentobject.type === "image") {
+								return (
+									<img
+										className="single-blog-image"
+										src={require("assets/img/blog/BlogImages/" +
+											contentobject.src)}
+										key={`img${index}`}
+									></img>
+								);
+							} else if (contentobject.type === "h2") {
+								return (
+									<h2
+										className="blog-secondary-heading text-left"
+										key={`h2${index}`}
+									>
+										{contentobject.content}
+									</h2>
+								);
+							} else if (contentobject.type === "list-block") {
+								return (
+									<div
+										className="blog-list-block"
+										key={`lb${index}`}
+									>
+										<h3 className="blog-list-heading">
+											{contentobject.listHeading}
+										</h3>
+										{contentobject.listItems.map(
+											(listItem, index) => {
+												return (
+													<li
+														className="blog-list-item"
+														key={`bli${index}`}
+													>
+														{listItem}
+													</li>
+												);
+											}
+										)}
+									</div>
+								);
+							} else {
+								return;
+							}
+						})}
+					</div>
+				</div>
+			);
+		} else {
+			return;
+		}
+	});
+
+	return (
+		<div className="mobile-responsive">
+			<ExamplesNavbar activePage="/blogs" />
+			{isLoading ? <Spinner /> : blogContent}
+		</div>
+	);
 }
 
-export default BlogsPage;
+export default SingleBlog;
