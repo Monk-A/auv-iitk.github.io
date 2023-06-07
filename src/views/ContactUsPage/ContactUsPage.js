a651dd00120091186cf82377c6c777c98c6aa84c
Sachin24704 <102963346+Sachin24704@users.noreply.github.com>
2023-06-07 17:20:18 +0530

    Changed ContactUsPage.js to a function based component (#131)

--- src/views/ContactUsPage/ContactUsPage.js
+++ src/views/ContactUsPage/ContactUsPage.js
@@ -1,167 +1,152 @@
-import React, { Component } from "react";
-import { Button, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap'
+import React, { useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Row,
+} from 'reactstrap'
 import axios from "../../axios-contact";
 import ExamplesNavbar from "../../components/Navbars/ExamplesNavbar";
 import "./ContactUsPage.css"
-class ContactUsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      message: "",
-      email: ""
-    };
-    this.postDataHandler = this.postDataHandler.bind(this);
-    this.nameChangedHandler = this.nameChangedHandler.bind(this);
-    this.messageChangedHandler = this.messageChangedHandler.bind(this);
-    this.emailChangedHandler = this.emailChangedHandler.bind(this);
-  }
 
-  componentDidMount() {
-    window.scrollTo(0, 0);
-    document.documentElement.classList.remove("nav-open");
-  }
+const ContactUsPage = () => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
 
-  postDataHandler(event) {
+  const postDataHandler = (event) => {
     event.preventDefault();
-    // console.log("Post Data Handler Called");
-    const Data = {
-      name: this.state.name,
-      message: this.state.message,
-      email: this.state.email
+    const data = {
+      name: name,
+      message: message,
+      email: email
     }
-    axios.post('/message.json', Data).then(response => {
-      // eslint-disable-next-line no-console
+    axios.post('/message.json', data).then(response => {
       console.log(response);
     })
 
-    this.setState({
-      name: "",
-      email: "",
-      message: ""
-    })
+    setName("");
+    setEmail("");
+    setMessage("");
   }
 
-  nameChangedHandler(event) {
+  const nameChangedHandler = (event) => {
     event.preventDefault();
-    this.setState({
-      name: event.target.value
-    })
+    setName(event.target.value);
   }
 
-  messageChangedHandler(event) {
+  const messageChangedHandler = (event) => {
     event.preventDefault();
-    this.setState({
-      message: event.target.value
-    })
+    setMessage(event.target.value);
   }
 
-  emailChangedHandler(event) {
+  const emailChangedHandler = (event) => {
     event.preventDefault();
-    this.setState({
-      email: event.target.value
-    })
+    setEmail(event.target.value);
   }
 
-  render() {
-    return (
-      <>
-        <ExamplesNavbar activePage="/contact-us" />
-        <div className="section text-center ">
-          <Container className="reduce-margin">
+  return (
+    <>
+      <ExamplesNavbar activePage="/contact-us" />
+      <div className="section text-center ">
+        <Container className="reduce-margin">
+          <Row>
+            <h2 className="heading-contact-us" style={{ textAlign: "center", zIndex: "10" }}>CONTACT US</h2>
+          </Row>
+        </Container>
+      </div>
+      <Container className="main">
+        <div className="section landing-section">
+          <Container className="u-border contact-us-section">
             <Row>
-              <h2 className="heading-contact-us" style={{ textAlign: "center", zIndex:"10" }}>CONTACT US</h2>
+              <Col lg="6" className="contact-us-form-container">
+                <h2 className=" heading-small-contact-us">Share Feedback or Ask your Queries </h2>
+                <Form className="contact-form" onSubmit={postDataHandler}>
+                  <Row>
+                    <Col md="12">
+                      <label className="mb-0 desc-auv">Name</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-single-02" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          placeholder="Name"
+                          type="text"
+                          value={name}
+                          onChange={nameChangedHandler} />
+                      </InputGroup>
+                    </Col>
+                    <Col md="12">
+                      <label className="mb-0 desc-auv">Email</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-email-85" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          placeholder="Email"
+                          type="email"
+                          value={email}
+                          onChange={emailChangedHandler} />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <label className="mb-0 desc-auv">Message</label>
+                  <Input
+                    placeholder="Tell us your thoughts and feelings..."
+                    type="textarea"
+                    rows="4"
+                    value={message}
+                    onChange={messageChangedHandler}
+                  />
+                  <Button className="contact-us-form-button">
+                    Send
+                  </Button>
+                </Form>
+              </Col>
+              <Col lg="6" className="contact-us-form-container">
+                <h2 className="ml-5 small-heading">Contact details</h2>
+                <div className="row u-margin-top-large">
+                  <Col md="1">
+                    <i className="fa fa-phone contact-icon" />
+                  </Col>
+                  <Col md="10" className="ml-3 mt-4">
+                    <p className="desc-auv">Phone Number : +91 9521597992 </p>
+                  </Col>
+
+                  <Col md="1">
+                    <i className="fa fa-envelope-square contact-icon" />
+                  </Col>
+                  <Col md="10" className="ml-3 mt-4">
+                    <p className="desc-auv">Email id : iitkauv@gmail.com </p>
+                  </Col>
+
+                  <Col md="1">
+                    <i className="fa fa-map-marker contact-icon" />
+                  </Col>
+                  <Col md="10" className="ml-3 mt-4">
+                    <p className="desc-auv">
+                      Address : AUV Room, Hall of Residence 2, Indian
+                      Institute of Technology, Kanpur, Uttar Pradesh, India -
+                      208016{" "}
+                    </p>
+                  </Col>
+                </div>
+              </Col>
             </Row>
           </Container>
         </div>
-        <Container className="main">
-          <div className="section landing-section">
-            <Container className="u-border contact-us-section">
-              <Row>
-                <Col lg="6" className="contact-us-form-container">
-                  <h2 className=" heading-small-contact-us">Share Feedback or Ask your Queries </h2>
-                  <Form className="contact-form" onSubmit={this.postDataHandler}>
-                    <Row>
-                      <Col md="12">
-                        <label className="mb-0 desc-auv">Name</label>
-                        <InputGroup>
-                          <InputGroupAddon addonType="prepend">
-                            <InputGroupText>
-                              <i className="nc-icon nc-single-02" />
-                            </InputGroupText>
-                          </InputGroupAddon>
-                          <Input
-                            placeholder="Name"
-                            type="text"
-                            value={this.state.name}
-                            onChange={this.nameChangedHandler} />
-                        </InputGroup>
-                      </Col>
-                      <Col md="12">
-                        <label className="mb-0 desc-auv">Email</label>
-                        <InputGroup>
-                          <InputGroupAddon addonType="prepend">
-                            <InputGroupText>
-                              <i className="nc-icon nc-email-85" />
-                            </InputGroupText>
-                          </InputGroupAddon>
-                          <Input
-                            placeholder="Email"
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.emailChangedHandler} />
-                        </InputGroup>
-                      </Col>
-                    </Row>
-                    <label className="mb-0 desc-auv">Message</label>
-                    <Input
-                      placeholder="Tell us your thoughts and feelings..."
-                      type="textarea"
-                      rows="4"
-                      value={this.state.message}
-                      onChange={this.messageChangedHandler}
-                    />
-                    <Button className="contact-us-form-button">
-                      Send
-                    </Button>
-                  </Form>
-                </Col>
-                <Col lg="6" className="contact-us-form-container">
-                  <h2 className="ml-5 small-heading">Contact details</h2>
-                  <div className="row u-margin-top-large">
-                    <Col md="1">
-                      <i className="fa fa-phone contact-icon" />
-                    </Col>
-                    <Col md="10" className="ml-3 mt-4">
-                      <p className="desc-auv">Phone Number : +91 9521597992 </p>
-                    </Col>
-
-                    <Col md="1">
-                      <i className="fa fa-envelope-square contact-icon" />
-                    </Col>
-                    <Col md="10" className="ml-3 mt-4">
-                      <p className="desc-auv">Email id : iitkauv@gmail.com </p>
-                    </Col>
-
-                    <Col md="1">
-                      <i className="fa fa-map-marker contact-icon" />
-                    </Col>
-                    <Col md="10" className="ml-3 mt-4">
-                      <p className="desc-auv">
-                        Address : AUV Room, Hall of Residence 2, Indian
-                        Institute of Technology, Kanpur, Uttar Pradesh, India -
-                        208016{" "}
-                      </p>
-                    </Col>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </Container>
-      </>
-    );
-  }
+      </Container>
+    </>
+  );
 }
 
 export default ContactUsPage;
