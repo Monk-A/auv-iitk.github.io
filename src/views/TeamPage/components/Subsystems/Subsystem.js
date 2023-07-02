b35a63dd2bb26d0298551437e4c1af677677d6ff
AMAN MALIK <115094204+Monk-A@users.noreply.github.com>
2023-07-02 16:00:46 +0530

    Update Subsystem.js
    The subsystem data is stored in an array called subsystemsData with each object containing the title, description, and link for each subsystem.
    The subsystemsData array is mapped over to dynamically generate the subsystem cards.
    The key attribute is added to each Col component to provide a unique identifier for React to efficiently update the DOM when the array changes.
    The unnecessary ml-auto and mr-auto classes in the Col component have been removed for cleaner code.
    The CSS classes and styles have been left unchanged as they are specific to the styling requirements of the project.

--- src/views/TeamPage/components/Subsystems/Subsystem.js
+++ src/views/TeamPage/components/Subsystems/Subsystem.js
@@ -3,105 +3,60 @@ import { Link } from "react-router-dom";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import "./Subsystems.css";
 
-function SubS() {
+function SubSystems() {
+  const subsystemsData = [
+    {
+      title: "MECHANICAL",
+      description:
+        "The mechanical subsystem is responsible for design and manufacture of the vehicle...",
+      link: "/mechanical",
+    },
+    {
+      title: "ELECTRICAL",
+      description:
+        "The Electrical Subsystem builds the framework powering our AUV consisting of Power Distribution System and the Monitoring system...",
+      link: "/electrical",
+    },
+    {
+      title: "SOFTWARE",
+      description:
+        "The software subsystem develops the algorithms controlling the robot and making it autonomous...",
+      link: "/software",
+    },
+    {
+      title: "BUSINESS",
+      description:
+        "The Business subsystem manages the fundings and outreach activities of the team...",
+      link: "/business",
+    },
+  ];
+
   return (
     <div className="section team-data landing-section">
       <Container>
         <Row className="d-flex justify-content-center">
-          <Col className="ml-auto mr-auto" md="12">
+          <Col md="12">
             <h2 className="text-center heading-main">Subsystems</h2>
           </Col>
         </Row>
         <Row>
-          <Col className="container text-center">
-            <Card className="card-plain bg-light card-subsystems">
-              <div className="row w-100 justify-content-center font-weight-bold h5">
-                MECHANICAL 
-                {/* <i class="fa fa-cogs fa-md"></i> */}
-              </div>
-              <p className="hide" style={{ fontSize: "16px" }}>
-                The mechanical subsystem is responsible for design and
-                manufacture of the vehicle. We design the entire vehicle and
-                accessories - working on Fluid Dynamics, Actuator controls, and
-                Pneumatics. We plan, simulate and test the physical structure of
-                the AUV and then manufacture it using state of art manufacturing
-                techniques. We use latest software such as SolidWorks and ANSYS
-                to make the design robust, modular, light and strong.
-              </p>
-              <Link to="/mechanical">
-                <p className="redirect">Know More</p>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="container text-center">
-            <Card className="card-plain bg-light card-subsystems">
-              <div className="row w-100 justify-content-center font-weight-bold h5">
-                ELECTRICAL 
-                {/* <i class="fa fa-bolt fa-md"></i> */}
-              </div>
-              <p className="hide">
-                The Electrical Subsystem builds the framework powering our AUV
-                consisting of Power Distribution System and the Monitoring
-                system. Arduino Mega is used to control the actuation and read
-                data from various sensors which in turn is controlled by an
-                Intel NUC. We work on the electrical aspects of the AUV,
-                providing power, driving actuators and interfacing with various
-                sensors on board. We build custom PCBs and act as an interface
-                between software and mechanical subsystems.
-              </p>
-              <Link to="/electrical">
-                <p className="redirect">Know More</p>
-              </Link>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="container text-center">
-            <Card className="card-plain bg-light card-subsystems">
-              <div className="row w-100 justify-content-center font-weight-bold h5">
-                SOFTWARE 
-                {/* <i class="fa fa-code fa-md" /> */}
-              </div>
-              <p className="hide">
-                The software subsystem develops the algorithms controlling the
-                robot and making it autonomous. We use latest cutting edge
-                technologies in the world of robotics software. The software
-                subsytem carries out simulations Gazebo and UWSim to ensure that
-                things work smoothly before actual testing.The main aim of
-                software subsystem is to write code, that is generic and error
-                free, in order efficiently coordinate the vehicles motion and
-                enable it perform at full potential.
-              </p>
-              <Link to="/software">
-                <p className="redirect">Know More</p>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="container text-center">
-            <Card className="card-plain bg-light card-subsystems">
-              <div className="row w-100 justify-content-center font-weight-bold h5">
-                BUSINESS &nbsp; 
-                {/* <i class="fa fa-briefcase fa-md"></i> */}
-              </div>
-              <p className="hide">
-                The Business subsystem manages the fundings and outreach
-                activities of the team. We manage the expenditures and fundings
-                of the team. We represent the team before our sponsors and try
-                to procure them. We maintain the team website and pages on the
-                different social media platforms. The main aim of the business
-                subsystem is to ensure that the team gets sufficient fundings
-                and has an excellent outreach to ensure the smooth functioning
-                of the other three subsystems.
-              </p>
-              <Link to="/business">
-                <p className="redirect">Know More</p>
-              </Link>
-            </Card>
-          </Col>
+          {subsystemsData.map((subsystem) => (
+            <Col className="container text-center" key={subsystem.title}>
+              <Card className="card-plain bg-light card-subsystems">
+                <div className="row w-100 justify-content-center font-weight-bold h5">
+                  {subsystem.title}
+                </div>
+                <p className="hide">{subsystem.description}</p>
+                <Link to={subsystem.link}>
+                  <p className="redirect">Know More</p>
+                </Link>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
   );
 }
 
-export default SubS;
+export default SubSystems;
